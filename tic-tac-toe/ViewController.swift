eacfac7d49370441050f76734fc77cae1eed4059
olzu0217 <olga.zubrilina@netcracker.com>
2019-12-30 17:20:15 +0300

    Initial Commit

---
+++ tic-tac-toe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tic-tac-toe
+//
+//  Created by Olga Zubrilina on 30.12.2019.
+//  Copyright © 2019 Olga Zubrilina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
